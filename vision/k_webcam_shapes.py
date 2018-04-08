14715519a6895dc6c4a79ef76da6b8a1154399fc
mathmaniac43 <mathmaniac43@gmail.com>
2018-04-08 16:37:28 -0400

    Added sliders to adjust detection settings. Can fairly reliably detect differently-colored squares.

--- vision/k_webcam_shapes.py
+++ vision/k_webcam_shapes.py
@@ -2,13 +2,42 @@ import argparse
 import cv2
 import imutils
 import math
+import time
 
 from pyimagesearch.shapedetector import ShapeDetector
 
-camera = cv2.VideoCapture(0)
+mouse_x, mouse_y = -1, -1
+def mouse_callback(event, x, y, flags, param):
+    global mouse_x, mouse_y
+    
+    if event == cv2.EVENT_MOUSEMOVE:
+        mouse_x = x
+        mouse_y = y
+        
+def trackbar_callback(val):
+    pass
+        
+IMAGE_WIDTH = 640
+IMAGE_HEIGHT = 480
+WINDOW_NAME = "Image"
+CANNY_MIN_TRACKBAR_NAME = "Canny Min"
+CANNY_MAX_TRACKBAR_NAME = "Canny Max"
+AREA_MIN_TRACKBAR_NAME = "Area Min"
+AREA_MAX_TRACKBAR_NAME = "Area Max"
+
+cv2.namedWindow(WINDOW_NAME)
+cv2.setMouseCallback(WINDOW_NAME, mouse_callback)
+cv2.createTrackbar(CANNY_MIN_TRACKBAR_NAME, WINDOW_NAME, 120, 255, trackbar_callback)
+cv2.createTrackbar(CANNY_MAX_TRACKBAR_NAME, WINDOW_NAME, 180, 255, trackbar_callback)
+cv2.createTrackbar(AREA_MIN_TRACKBAR_NAME,  WINDOW_NAME, 70,  200, trackbar_callback)
+cv2.createTrackbar(AREA_MAX_TRACKBAR_NAME,  WINDOW_NAME, 90,  200, trackbar_callback)
+
+camera = cv2.VideoCapture(1)
 camera.set(cv2.cv.CV_CAP_PROP_FPS, 2)
-camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,  640)
-camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,  IMAGE_WIDTH)
+camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, IMAGE_HEIGHT)
+
+time.sleep(2)
 
 sd = ShapeDetector()
 while True:
@@ -18,68 +47,71 @@ while True:
     
     (grabbed, image) = camera.read()
     if not grabbed:
-        break
+        continue
         
-    image = cv2.flip(image, 1)
+    canny_min = cv2.getTrackbarPos(CANNY_MIN_TRACKBAR_NAME, WINDOW_NAME)
+    canny_max = cv2.getTrackbarPos(CANNY_MAX_TRACKBAR_NAME, WINDOW_NAME)  
+    area_min = cv2.getTrackbarPos(AREA_MIN_TRACKBAR_NAME, WINDOW_NAME)  
+    area_max = cv2.getTrackbarPos(AREA_MAX_TRACKBAR_NAME, WINDOW_NAME)
     
-    resized = image
-    ratio = 1
-    #resized = imutils.resize(image, width=500)
-    #ratio = image.shape[0] / float(resized.shape[0])
+    image = cv2.flip(image, -1)
     
-    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow("gray", gray)
-    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.bilateralFilter(gray, 11, 17, 17)
-    #cv2.imshow("blurred", blurred)
     
-    edged = cv2.Canny(blurred, 30, 200)
-    #(contours, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    edged = cv2.Canny(blurred, canny_min, canny_max)
     (contours, _) = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key = cv2.contourArea, reverse = False)
     
-    #shapes = {'circle':[], 'triangle':[], 'square':[], 'rectangle':[], 'pentagon':[]}
-    # loop over the contours
+    if (mouse_x >= 0 and mouse_x < IMAGE_WIDTH and
+        mouse_y >= 0 and mouse_y < IMAGE_HEIGHT):
+        
+        (mouse_r, mouse_g, mouse_b) = image[mouse_y][mouse_x]
+        
+        s = ("x=" + str(mouse_x) + " " +
+             "y=" + str(mouse_y) + " " +
+             "r=" + str(mouse_r) + " " +
+             "g=" + str(mouse_g) + " " +
+             "b=" + str(mouse_b))
+        
+        # Print color information where mouse is
+        cv2.putText(image, s, (5, IMAGE_HEIGHT - 5), cv2.FONT_HERSHEY_SIMPLEX,
+            0.5, (255, 255, 255), 2)
+    
+    # Loop over the contours
     for c in contours:
-	    M = cv2.moments(c)
-	    if M["m00"] == 0:
-	        continue
-	    
-	    cX = int(math.floor((M["m10"] / M["m00"]) * ratio))
-	    cY = int(math.floor((M["m01"] / M["m00"]) * ratio))
-	    
-	    if cX < 0 or cX >= len(resized) or cY < 0 or cY >= len(resized[0]):
-	        continue
-		    
-	    (r, g, b) = image[cX][cY]
-	    
-	    # only continue if reddish
-	    if not (r > 100 and (r > g or r > b)):
-	        continue
-	    
-	    shape = sd.detect(c)
-	    
-	    # only continue if squareish
-	    #if not shape == 'square':
-	    #    continue
-	    
-	    # only continue if rectangularish
-	    if not shape == 'rectangle':
-	        continue
-	    
-	    print((r, g, b))
-	    
-	    #shapes[shape].append((cX, cY))
-	    c = c.astype("float")
-	    c *= ratio
-	    c = c.astype("int")
-	    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-	    cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-		    0.5, (255, 255, 255), 2)
-	        
-
-    # show the output image
-    cv2.imshow("Image", image)
+        M = cv2.moments(c)
+        if M["m00"] == 0:
+            continue
+        
+        x = int(math.floor((M["m10"] / M["m00"])))
+        y = int(math.floor((M["m01"] / M["m00"])))
+        if (x < 0 or x >= IMAGE_WIDTH or
+            y < 0 or y >= IMAGE_HEIGHT):
+            continue
+        
+        area = cv2.contourArea(c)
+        if (area < area_min or area > area_max):
+            continue
+        
+        shape = sd.detect(c)
+        if not shape == 'square':
+            continue
+            
+        (r, g, b) = image[y][x]
+        
+        s = ("x=" + str(x) + " " +
+             "y=" + str(y) + " " +
+             "r=" + str(r) + " " +
+             "g=" + str(g) + " " +
+             "b=" + str(b) + " " +
+             "A=" + str(area))
+        
+        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        cv2.putText(image, s, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+            0.5, (255, 255, 255), 2)
+    
+    cv2.imshow(WINDOW_NAME, image)
 
 camera.release()
 cv2.destroyAllWindows()
