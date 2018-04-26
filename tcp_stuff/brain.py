c84d0a064a0221365d5e346b1e1490ef94d3ac19
labpc <labpc@jhu.edu>
2018-04-25 21:35:27 -0400

    update kitchennet.py to play with theClients/very_rudimentary_client.py

--- tcp_stuff/brain.py
+++ tcp_stuff/brain.py
@@ -4,24 +4,22 @@ import time
 
 sV = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-sV_address = ('127.0.0.1', 8001)
+vSocket = 8001
+sV_address = ('127.0.0.1', vSocket)
 #s2_address = ('localhost', 8001)
-
 sV.bind(sV_address)
 #s2.bind(s2_address)
-
 sV.listen(1)
-
-
 cV, cV_address = sV.accept()
-
 print(cV.recv(4096))
 
 while True:
-    time.sleep(5)
     
-    print 'trying 8000'
+    '''
+    Check Vision Client first
+    '''
+    
+    print 'trying Vision on %d' % vSocket
     try:
         cV.sendall('k')
         s = cV.recv(4096)
@@ -29,5 +27,6 @@ while True:
         
     finally:
         print 'oops'
+
 
 cV.close()
