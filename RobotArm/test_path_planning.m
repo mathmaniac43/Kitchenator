7bdea82c76eea0822bf2165f04cf4177e21f3a49
Bryanna Yeh <bryanna.yeh@jhuapl.edu>
2018-04-26 18:42:08 -0400

    test of robot toolbox path planning

--- RobotArm/test_path_planning.m
+++ RobotArm/test_path_planning.m
@@ -1,10 +1,18 @@
-map = robotics.BinaryOccupancyGrid(10,10,.5)
+base_radius = 0.15;
+resolution = 100;
+world_x = 0.7;
+world_y = 1;
+
+map = robotics.BinaryOccupancyGrid(world_x,world_y,resolution);
+setOccupancy(map, [0 world_y/2], 1);
+inflate(map, base_radius);
+y = 0:1/resolution:world_y;
+x = zeros(size(y));
+setOccupancy(map, [x' y'], 1);
 show(map)
-setOccupancy(map,[0,0],1)
-setOccupancy(map,[5,5],.5)
-show(map)
-prm = robotics.PRM
-prm.Map = map
-prm.NumNodes = 50
-p = findpath(prm, [3,0], [5,8])
+
+prm = robotics.PRM;
+prm.Map = map;
+prm.NumNodes = 100;
+p = findpath(prm, [.1 0],[.3 .9]);
 show(prm)
