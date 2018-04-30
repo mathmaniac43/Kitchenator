5520463d47316d35a6ef180acaf000bb8aec7a9a
Manizade <s.drummer76@gmail.com>
2018-04-30 00:35:34 -0400

    delet tee cee pee stuff, add option to matlab client

--- theClients/matlabClient.m
+++ theClients/matlabClient.m
@@ -1,7 +1,8 @@
 % For an arm client, for example
 while true
     url = 'http://127.0.0.1:8080/getArmGoals';
-    data = webread(url); % Get dat string
+    options = weboptions('RequestMethod', 'get'); % could also be 'post'
+    data = webread(url, options); % Get dat string
     jsonData = jsondecode(data)
     pause(5)
 end
